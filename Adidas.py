388a1203876372ede16d281f49fb20325954a40d
Lewis Jenner <ljenner00@gmail.com>
2018-08-03 16:31:08 +0100

    login

--- Adidas.py
+++ Adidas.py
@@ -4,7 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException        
 
 def login():
     browser = webdriver.Chrome()
-    browser.get((''))
+    browser.get(('https://www.adidas.co.uk/'))
+
+    try:
+        browser.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[1]/div/div[1]/div/div/a[3]').click()
+    except NoSuchElementException:
+        pass
+    
+
+if __name__ == "__main__":
+    login()
